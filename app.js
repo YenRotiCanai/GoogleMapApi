f7a5b17adceffc140f0f7a83ef661abe66b4f0b2
YenRotiCanai <lnight94@gmail.com>
2021-10-09 21:55:49 +0800

    js change

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 function initMap(){
     
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+
     //地圖設定
     var options = {
-        center: {lat: 24.794387044191037 , lng: 121.00012748250742},
+        center: {lat:24.82434674639735, lng:121.00141682492709},
         zoom: 12
     }
 
     //創新的地圖
-    map = new google.maps.Map(document.getElementById('map'), options)
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    directionsRenderer.setMap(map);
 
     class Rest_Info{
         constructor(location, name, icon)
@@ -24,56 +28,6 @@ function initMap(){
     const R3_jiazhen =  new Rest_Info({lat:24.799638469920698, lng: 120.95316545574171}, `<h2>家蓁素食自助餐</h2>`, "/icon/Picture3_jia.png");
     const R4_zixin = new Rest_Info({lat:24.871209418461, lng: 120.993891846625}, `<h2>子欣素食</h2>`, "/icon/Picture4_zi.png");
 
-    
-
-    /*
-    //標記出餐廳的位子
-    const marker1 = new google.maps.Marker({
-        position: R1_yashuzhai_loc,
-        map: map,
-    });
-    const marker2 = new google.maps.Marker({
-        position: R2_jingding_loc,
-        map: map,
-    });
-    const marker3 = new google.maps.Marker({
-        position: R3_jiazhen_loc,
-        map: map,
-    });
-    const marker4 = new google.maps.Marker({
-        position: R4_zixin_loc,
-        map: map,
-    });
-    
-
-    //顯示餐廳名字
-    const detailWindow1 = new google.maps.InfoWindow({
-        content: `<h2>雅素齋自然人文蔬食館</h2>`
-    });
-    const detailWindow2 = new google.maps.InfoWindow({
-        content: `<h2>井町日式蔬食料理</h2>`
-    });
-    const detailWindow3 = new google.maps.InfoWindow({
-        content: `<h2>家蓁素食自助餐</h2>`
-    });
-    const detailWindow4 = new google.maps.InfoWindow({
-        content: `<h2>子欣素食</h2>`
-    });
-
-    marker1.addListener("click", ()=>{
-        detailWindow1.open(map, marker1);
-    })
-    marker2.addListener("click", ()=>{
-        detailWindow2.open(map, marker2);
-    })
-    marker3.addListener("click", ()=>{
-        detailWindow3.open(map, marker3);
-    })
-    marker4.addListener("click", ()=>{
-        detailWindow4.open(map, marker4);
-    })
-*/
-
     function addMarker(property){
         var icon = {
             url:property.icon,
@@ -89,7 +43,8 @@ function initMap(){
         const detailWindow = new google.maps.InfoWindow({
             content: property.name
         });
-        marker.addListener("click", ()=>{
+        marker.addListener("click", (mapsMouseEvent)=>{
+            detailWindow.close();
             detailWindow.open(map, marker);
         })
     }
@@ -98,4 +53,8 @@ function initMap(){
     addMarker(R2_jingding);
     addMarker(R3_jiazhen);
     addMarker(R4_zixin);
+}
+
+function calcRoute(){
+    var start = 
 }
